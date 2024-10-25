ff20a5bd37076a48ba80fdb75fb0fb9312077194
Na3r0 <96481684+Na3r0@users.noreply.github.com>
2024-10-25 12:52:25 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,220 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const EVOLUTION_URL = "https://pokeapi.co/api/v2/evolution-chain/";
+let counterOffset = 0;
+let counterLimit = 10;
+let addPokemonHTMLExists = false;
+let addSearchHTMLExists = false;
+let localStorageCounterVar = localStorage.setItem("counterVar", 0);
+let amountInput = document.getElementById('amount-input');
+  if (amountInput) {
+  document.getElementById('amount-input').onchange = function() {addPokemonAmount()};}
+
+
+function clearContent() {
+  contentId = document.getElementById("content");
+  contentId.innerHTML = "";
+  addPokemonHTMLExists = false;
+  addSearchHTMLExists = false;
+}
+
+function clearStorage() {
+  let localStorageCounterVar = localStorage.getItem("counterVar");
+  localStorageCounterVar = parseInt(localStorageCounterVar);
+  counterLimit -= localStorageCounterVar;
+}
+
+function addPokemonAmount() {
+  let amount = document.getElementById('amount-input');
+  if (amount.value < 10 || amount.value > 100) {
+    console.log('Only 10 - 100 is allowed');
+  }
+  else {
+  counterLimit = amount.value;
+  clearContent();
+  getData();
+}
+}
+
+
+async function searchPokemon() {
+  const query = document.getElementById("search-input").value.toLowerCase();
+  if (query.length === 0) {
+      clearContent();
+      getData();
+      return;   
+  }
+  try {
+      const response = await fetch(`${BASE_URL}/${query}`);
+      console.log(response);
+      if (!response.ok) {
+          throw new Error('Pokémon not found');
+      }
+      const pokemon = await response.json();
+      clearContent();
+      pokemonGeneralHTML(response.url, pokemon);
+
+  }   catch (error) {
+      console.error('Error fetching Pokémon:', error.message);
+      document.getElementById("content").innerHTML = /*html*/`
+      <div class="error-container">
+      <p>Pokémon not found</p>
+      <button id="backbtn" onclick="getData(), clearContent()">BACK</button>
+      </div>`;
+  }
+}
+
+async function getData() {
+  try {
+    const response = await fetch(
+      BASE_URL + `?limit=${counterLimit}&offset=${counterOffset}`
+    );
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    loadData(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function loadData(json) {
+  if (!addSearchHTMLExists) {
+    addSearchHTML();
+  }
+  getPokemonForLoop(json);
+  if (!addPokemonHTMLExists) {
+    addFooterHTML();
+  }
+}
+
+async function getPokemonForLoop(json) {
+  const pokemonPromises = json.results.map(async (pokemonData) => {
+    const response = await fetch(pokemonData.url);
+    const pokemon = await response.json();
+    return {url: pokemonData.url, 
+            data: pokemon };
+  });
+  const allPokemonData = await Promise.all(pokemonPromises);
+  allPokemonData.sort((a, b) => a.data.id - b.data.id);
+  allPokemonData.forEach((pokemonObject) => {
+    setTimeout(() => pokemonHTML(pokemonObject.url, pokemonObject.data), 100);
+  });
+}
+
+async function getPokemon(POKEMON) {
+  let response = await fetch(POKEMON);
+  let responseUrl = response.url;
+  let responseToJson = await response.json();
+  setTimeout(() => pokemonHTML(responseUrl, responseToJson), 50);
+}
+
+async function openPokemonGeneral(pokemonUrl) {
+  let response = await fetch(pokemonUrl);
+  let responseToJson = await response.json();
+  pokemonGeneralHTML(pokemonUrl, responseToJson);
+}
+
+async function chooseBackgroundColor(URL) {
+  try {
+    let response = await fetch(URL);
+    let responseToJson = await response.json();
+    const typeColorMap = {
+      grass: "rgb(119,204,85)",
+      bug: "rgb(170,187,33)",
+      fire: "rgb(255,68,34)",
+      normal: "rgb(170,170,153)",
+      water: "rgb(52,153,255)",
+      electric: "rgb(254,204,51)",
+      poison: "rgb(245, 12, 245)",
+      ground: "rgb(222,187,85)",
+      fighting: "rgb(187,85,68)",
+      psychic: "rgb(255,85,153)",
+      flying: "rgb(136,153,255)",
+      ice: "rgb(102,204,255)",
+      rock: "rgb(187,170,102)",
+      ghost: "rgb(102,102,187)",
+      dragon: "rgb(119,101,238)",
+      dark: "rgb(119,85,68)",
+      steel: "rgb(170,170,187)",
+      fairy: "rgb(238,153,238)",
+    };
+
+    const pokemonId = document.getElementById(URL);
+    const pokemonType = responseToJson.types[0].type.name;
+    pokemonId.style.backgroundColor = typeColorMap[pokemonType];
+  } catch (error) {
+    console.error("Error fetching or applying background color:", error);
+  }
+}
+
+async function openPokemonStats(pokemonUrl) {
+  let response = await fetch(pokemonUrl);
+  let responseToJson = await response.json();
+  pokemonStatsHTML(pokemonUrl, responseToJson);
+}
+
+async function openPokemonEvolution(pokemonUrl) {
+  let response = await fetch(pokemonUrl);
+  let responseToJson = await response.json();
+  getEvolutionChains(pokemonUrl, responseToJson);
+}
+
+function addPokemon() {
+  let localStorageCounterVar = localStorage.getItem("counterVar");
+  localStorageCounterVar = parseInt(localStorageCounterVar);
+  counterLimit = parseInt(counterLimit);
+  counterOffset = localStorageCounterVar;
+  counterOffset += counterLimit;
+  localStorageCounterVar += counterLimit;
+  localStorage.setItem("counterVar", localStorageCounterVar);
+  console.log(
+    "CounterLimit ist:" + counterLimit,
+    "CounterOffset ist:" + counterOffset
+  );
+  getData();
+  setTimeout(() => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }, 500);
+  
+}
+
+async function getEvolutionChains(POKEMONURL, POKEMON) {
+  let response = await fetch(EVOLUTION_URL + `?limit=1000&offset=0`);
+  let responseToJson = await response.json();
+  for (let evolutionIndex = 0; evolutionIndex < responseToJson.results.length; evolutionIndex++) {
+    let evolutionChain = responseToJson.results[evolutionIndex];
+    getEvolution(POKEMONURL, POKEMON, evolutionChain.url);
+  }
+}
+
+async function getEvolution(POKEMONURL, POKEMON, URL) {
+  let response = await fetch(URL);
+  if (!response.ok) {  
+      throw new Error(`URL not found or invalid (Status: ${response.status})`);
+  }
+  let responseToJson = await response.json();
+  let firstEvoPokemon = responseToJson.chain.species;
+  let secondEvoPokemon = responseToJson.chain.evolves_to.length > 0 ? responseToJson.chain.evolves_to[0].species : null;
+  let thirdEvoPokemon = (secondEvoPokemon && responseToJson.chain.evolves_to[0].evolves_to.length > 0) ? responseToJson.chain.evolves_to[0].evolves_to[0].species : null;
+  if (firstEvoPokemon.name === POKEMON.name || (secondEvoPokemon && secondEvoPokemon.name === POKEMON.name) || 
+    (thirdEvoPokemon && thirdEvoPokemon.name === POKEMON.name)) {
+      let firstEvoPokemonURL = await fetch(`${BASE_URL}/${firstEvoPokemon.name}`);
+      let first = await firstEvoPokemonURL.json();
+      let second = null;
+      if (secondEvoPokemon) {
+          let secondEvoPokemonURL = await fetch(`${BASE_URL}/${secondEvoPokemon.name}`);
+          second = await secondEvoPokemonURL.json();
+      }
+      let third = null;
+      if (thirdEvoPokemon) {
+          let thirdEvoPokemonURL = await fetch(`${BASE_URL}/${thirdEvoPokemon.name}`);
+          third = await thirdEvoPokemonURL.json();
+      }
+      pokemonEvolutionHTML(POKEMONURL, POKEMON, first, second, third);
+  } 
+}
+
---
+++ scripts/template.js
@@ -0,0 +1,153 @@
+function addSearchHTML() {
+    const contentId = document.getElementById('content');
+    contentId.innerHTML += /*HTML*/`
+    <div id="search-input-container">
+        <input id="search-input" placeholder="Search for Pokemons ID/NAME">
+        <button onclick="searchPokemon()">SEARCH</button>
+    </div>`;
+    addSearchHTMLExists = true;
+}
+
+
+function addFooterHTML() {
+    const contentId = document.getElementById('content');
+    contentId.innerHTML += /*HTML*/`
+    <div id="button-add-container">
+        <button id="add-btn" onclick="addPokemon()">LOAD ${counterLimit} MORE</button>
+        <input id='amount-input' type="number" min="0" max="100"placeholder="10 - 100" onchange="addPokemonAmount()">
+    </div>
+    <div id="button-footer-container">
+        <a id="footer-link" href="#end">
+            <img src="./assets/icons/arrow-right-solid.svg" id="arrow-icon">
+        </a>
+    </div>`;
+    addPokemonHTMLExists = true;
+}
+
+
+function pokemonHTML(URL, pokemon) {
+    const contentId = document.getElementById('content');
+    let cardHTML = /*HTML*/`
+    <div class="card" id="${URL}" onclick="openPokemonGeneral('${URL}')">
+        <div class="card-top">
+            <span class="pokemon-name">${pokemon.name}</span>
+            <span class="pokemon-id">ID: ${pokemon.id}</span>
+        </div>
+        <div class="card-middle">`;
+        chooseBackgroundColor(URL);
+    for (let typeIndex = 0; typeIndex < pokemon.types.length; typeIndex++) {
+        cardHTML += /*HTML*/`
+            <span class="pokemon-type">${pokemon.types[typeIndex].type.name}</span>`;
+            
+    }
+    cardHTML += /*HTML*/`
+        </div>
+        <div class="pokemon-image-container">
+        <img class="pokemon-img" src="${pokemon.sprites.other['official-artwork'].front_default}">
+        </div>`;
+    contentId.innerHTML += cardHTML;
+    
+}
+
+
+function pokemonGeneralHTML(URL, pokemon) {
+    const contentId = document.getElementById('content');
+    clearContent();
+    contentId.innerHTML = /*HTML*/`
+    <div id="pokemon-big-container">
+        <div id="card-top-id" onclick="getData(), clearContent(), clearStorage()">
+            <span id="pokemon-name-id">${pokemon.name}</span>
+            <span id="pokemon-id-id">ID: ${pokemon.id}</span>
+        </div>
+        <div id="${URL}" class="width-100 d-flex pokemon-container" onclick="getData(), clearContent(), clearStorage()">
+            <img id="pokemon-img-id" src="${pokemon.sprites.other['official-artwork'].front_default}">
+        </div>
+        <div id="pokemon-weight-height">
+                <span class="phys-info" id="pokemon-weight">HEIGHT: ${pokemon.height}cm</span>
+                <span class="phys-info" id="pokemon-height">WEIGHT: ${pokemon.weight/10}kg</span>
+            </div>
+        <div id="card-bottom-id">
+            <span class="pokemon-info" id="general" onclick="openPokemonGeneral('${URL}')">General</span>
+            <span class="pokemon-info"id="stats" onclick="openPokemonStats('${URL}')">Stats</span>
+            <span class="pokemon-info" id="evolution" onclick="openPokemonEvolution('${URL}')">Evolution</span>
+        </div>
+    </div>`;
+    chooseBackgroundColor(URL);
+    addPokemonHTMLExists = false;
+    addSearchHTMLExists = false;
+    
+    let localStorageCounterVar = localStorage.getItem("counterVar");
+        localStorageCounterVar = parseInt(localStorageCounterVar);
+        counterLimit = localStorageCounterVar + counterLimit;
+        counterOffset = 0;
+}
+
+
+function pokemonStatsHTML(URL, pokemon) {
+    const contentId = document.getElementById('content');
+    clearContent();
+    contentId.innerHTML = /*HTML*/`
+        <div id="pokemon-big-container">
+            <div id="card-top-id" onclick="getData(), clearContent(), clearStorage()">
+                <span id="pokemon-name-id">${pokemon.name}</span>
+                <span id="pokemon-id-id">ID: ${pokemon.id}</span>
+            </div>
+            
+            <div id="pokemon-stats-container-id" onclick="getData(), clearContent(), clearStorage()">
+                <h2>BASE STATS</h2>
+                ${generateStatBar('HP', pokemon.stats[0].base_stat)}
+                ${generateStatBar('Attack', pokemon.stats[1].base_stat)}
+                ${generateStatBar('Defense', pokemon.stats[2].base_stat)}
+                ${generateStatBar('Sp. Atk', pokemon.stats[3].base_stat)}
+                ${generateStatBar('Sp. Def', pokemon.stats[4].base_stat)}
+                ${generateStatBar('Speed', pokemon.stats[5].base_stat)}
+            </div>
+            <div id="card-bottom-id">
+                <span class="pokemon-info" id="general" onclick="openPokemonGeneral('${URL}')">General</span>
+                <span class="pokemon-info" id="stats" onclick="openPokemonStats('${URL}')">Stats</span>
+                <span class="pokemon-info" id="evolution" onclick="openPokemonEvolution('${URL}')">Evolution</span>
+            </div>
+        </div>`;
+}
+
+
+function generateStatBar(statName, statValue) {
+    const maxStat = 255; 
+    const statPercentage = (statValue / maxStat) * 100;
+    return /*HTML*/`
+        <div class="pokemon-stat-container">
+            <span class="stat-label">${statName}: ${statValue}</span>
+            <div class="progress-bar">
+                <div class="progress" style="width: ${statPercentage}%;"></div>
+            </div>
+        </div>
+    `;
+}
+
+
+function pokemonEvolutionHTML(URL, pokemon, first, second, third) {
+    const contentId = document.getElementById('content');
+    clearContent();
+    let secondEvoHTML = second ? `<img class="evo-pok" src="${second.sprites.other['official-artwork'].front_default}" onclick="openPokemonGeneral('${BASE_URL}/${second.id}')">` : '';
+    let thirdEvoHTML = third ? `<img class="evo-pok" src="${third.sprites.other['official-artwork'].front_default}" onclick="openPokemonGeneral('${BASE_URL}/${third.id}')">` : '';
+    contentId.innerHTML = /*HTML*/`
+    <div id="pokemon-big-container">
+        <div id="card-top-id" onclick="getData(), clearContent(), clearStorage()">
+            <span id="pokemon-name-id">${pokemon.name}</span>
+            <span id="pokemon-id-id">ID: ${pokemon.id}</span>
+        </div>
+        <div id="pokemon-evo-container-id">
+            <img class="evo-pok" src="${first.sprites.other['official-artwork'].front_default}" onclick="openPokemonGeneral('${BASE_URL}/${first.id}')">
+            ${secondEvoHTML}
+            ${thirdEvoHTML}
+        </div>
+        <div id="card-bottom-id">
+            <span class="pokemon-info" id="general" onclick="openPokemonGeneral('${URL}')">General</span>
+            <span class="pokemon-info" id="stats" onclick="openPokemonStats('${URL}')">Stats</span>
+            <span class="pokemon-info" id="evolution" onclick="openPokemonEvolution('${URL}')">Evolution</span>
+        </div>
+    </div>`;
+}
+
+
+
